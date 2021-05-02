1c3235d99723e9b775a9d2e2f080047602a2230f
alxndrdrgz <alexander.e.rsa@gmail.com>
2021-05-02 13:12:45 -0400

    Added javascript pseudo code

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//Pseudo Code for The Grape Gatsby
+
+// 1 - Create namespace object
+
+// 2 - Create init function
+    // - Create variable for form
+    // - Add event listener:
+        // * On submit: create variable to store user's form input (i.e. their main dish)
+        // * take user's input to make a request to the API (i.e call the API call function and pass in the user's input as the argument)
+
+// 3 - Call init function
+// 4 - Make call to the API with a parameter that will act as a place holder for the user's form input
+    // * IF the API returns data, display the data on the page (i.e call the display function)
+    // * ELSE (no data return) run error handling function!!!!!
+
+//5 - Error handling function
+    // * if user lcicks without actually inputting any data in the form, alert them to do so
+    // * if user inputs an invalid string (i.e a main dish that doesn't retrun anything), print to the page that there are no results
+
 const apiKey = "440d6164874e4c1cadd56c5f2f61dcbd";
 
 const url = new URL('https://api.spoonacular.com/food/wine/pairing');
