66d33ab3d31534d03c86f2286f9a8043323215a6
alxndrdrgz <alexander.e.rsa@gmail.com>
2021-05-02 17:00:05 -0400

    cleaned up some pseudo code

--- script.js
+++ script.js
@@ -1,81 +1,81 @@
 //Pseudo Code for The Grape Gatsby
 
-// 1 - Create namespace object
-    const gatsbyApp = {}
-    // - Create variable for form
-    gatsbyApp.apiKey = "440d6164874e4c1cadd56c5f2f61dcbd";
-    gatsbyApp.url = new URL('https://api.spoonacular.com/food/wine/pairing');
-    gatsbyApp.form = document.querySelector('form');
-    gatsbyApp.input = document.querySelector('input');
-    gatsbyApp.wineResults = document.querySelector('.wine');
-    
-    
-// 5 - Define a method which will display data returned from API call
-    gatsbyApp.displayData = (pairedWines, pairingText) => {
-        // Iterate through wine pairing array
-        pairedWines.forEach((wine) => {
-            // console.log(wine);
-            // Create <li> that will hold each wine suggestion
-            const pairingOption = document.createElement('li');
-            // Write HTML that will be contained within <li> (pairingOption)
-            pairingOption.innerHTML = `
+//App name space object variable: 
+const gatsbyApp = {}
+gatsbyApp.apiKey = "440d6164874e4c1cadd56c5f2f61dcbd";
+gatsbyApp.url = new URL('https://api.spoonacular.com/food/wine/pairing');
+// Stored HTML elements variables
+gatsbyApp.form = document.querySelector('form');
+gatsbyApp.input = document.querySelector('input');
+gatsbyApp.wineResults = document.querySelector('.wine');
+
+
+// A function that displays the data returned from API call
+gatsbyApp.displayData = (pairedWines, pairingText) => {
+    // Iterates through wine pairing array
+    pairedWines.forEach((wine) => {
+        //<li> that will hold each wine suggestion
+        const pairingOption = document.createElement('li');
+        //HTML that will be contained within <li> (pairingOption)
+        pairingOption.innerHTML = `
                 <i class="fas fa-wine-glass-alt"></i>
                 <p>${wine}</p>
             `;
-            //Append newly created <li>s to the <ul> AKA wineResults
-            gatsbyApp.wineResults.append(pairingOption);
+        //Appends newly created <li>s to the <ul> AKA wineResults
+        gatsbyApp.wineResults.append(pairingOption);
 
-            const pairingDescription = document.querySelector('.results-text');
-
-            pairingDescription.innerHTML = `
+        //Stores div.results-text in a variable:
+        const pairingDescription = document.querySelector('.results-text');
+        //HTML that is generated within div.results-text
+        pairingDescription.innerHTML = `
             <h3>Wine Pairing Flavour Profile:</h3>
             <p>${pairingText}</p>`;
-        });
-    }
+    });
+}
 
-// 4 - Make call to the API with a parameter that will act as a place holder for the user's form input
-    // console.log(url);
-    gatsbyApp.getData = (userInput) => {
-        gatsbyApp.url.search = new URLSearchParams({
-            food: userInput,
-            apiKey: gatsbyApp.apiKey
+// A function that makes the initial API call to the browser
+gatsbyApp.getData = (userInput) => {
+    gatsbyApp.url.search = new URLSearchParams({
+        food: userInput,
+        apiKey: gatsbyApp.apiKey
+    })
+
+    fetch(gatsbyApp.url)
+        .then((response) => {
+            console.log(response);
+            return response.json();
         })
-
-        fetch(gatsbyApp.url)
-            .then((response) => {
-                console.log(response);
-                return response.json();
-            })
-            .then((jsonResponse) => {
-                console.log(jsonResponse);
-                //Call the displayData method
-                gatsbyApp.displayData(jsonResponse.pairedWines, jsonResponse.pairingText);
-            })
-    }
-    // * IF the API returns data, display the data on the page (i.e call the display function)
-    // * ELSE (no data return) run error handling function!!!!!
+        .then((jsonResponse) => {
+            console.log(jsonResponse);
+            //Calls the displayData method
+            gatsbyApp.displayData(jsonResponse.pairedWines, jsonResponse.pairingText);
+        })
+}
+// * IF the API returns data, display the data on the page (i.e call the display function)
+// * ELSE (no data return) run error handling function!!!!!
 
 
-// 2 - Create init function
-    gatsbyApp.init = () => {
-        // - Add event listener:
-        // * On submit: create variable to store user's form input (i.e. their main dish)
-        // * take user's input to make a request to the API (i.e call the API call function and pass in the user's input as the argument)
-        gatsbyApp.form.addEventListener('submit', (event) => {
-            //Prevent page refresh on submit
-            event.preventDefault();
-            // console.log('something has been submitted');
+//Defines the initialization function
+gatsbyApp.init = () => {
+    // Event listener
+    // * On submit: create variable to store user's form input (i.e. their main dish)
+    // * take user's input to make a request to the API (i.e call the API call function and pass in the user's input as the argument)
+    gatsbyApp.form.addEventListener('submit', (event) => {
+        //Prevent page refresh on submit
+        event.preventDefault();
 
-            gatsbyApp.userInput = gatsbyApp.input.value;
-            // console.log(userInput);
+        //Clear previous search results
 
-            // Clear form input
-            gatsbyApp.input.value = "";
+        //Stores user input in a variable
+        gatsbyApp.userInput = gatsbyApp.input.value;
 
-            //Call API request function (pass userInput as argument)
-            gatsbyApp.getData(gatsbyApp.userInput);
-        });
-    }
+        // Clear form input
+        gatsbyApp.input.value = "";
+
+        //Calls API request function (pass userInput as argument)
+        gatsbyApp.getData(gatsbyApp.userInput);
+    });
+}
 
 // 3 - Call init function
 gatsbyApp.init();
